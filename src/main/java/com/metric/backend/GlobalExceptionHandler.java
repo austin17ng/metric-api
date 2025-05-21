d8a3cbdf844a422751162d7b9ee58623175dcf6c
tungnguyen <tungnnnt17@gmail.com>
2025-05-21 14:39:54 +0700

    add logger

--- src/main/java/com/metric/backend/GlobalExceptionHandler.java
+++ src/main/java/com/metric/backend/GlobalExceptionHandler.java
@@ -5,11 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        logger.error("Error", e);
         ApiResponse<?> apiResponse = new ApiResponse<>();
         apiResponse.setStatus("error");
         apiResponse.setMessage(e.getMessage());
