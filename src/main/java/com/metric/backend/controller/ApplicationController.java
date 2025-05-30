dacc78d0f26a9c785bab8fceb0ffb6e1326beaef
tungnguyen <tungnnnt17@gmail.com>
2025-05-30 15:32:56 +0700

    update applications api doc

--- src/main/java/com/metric/backend/controller/ApplicationController.java
+++ src/main/java/com/metric/backend/controller/ApplicationController.java
@@ -2,6 +2,10 @@ package com.metric.backend.controller;
 
 import com.metric.backend.dto.ApplicationDto;
 import com.metric.backend.service.ApplicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/applications")
+@Tag(name = "Applications API", description = "Endpoints related to applications")
 public class ApplicationController {
 
     private final ApplicationService applicationService;
@@ -20,6 +25,11 @@ public class ApplicationController {
         this.applicationService = applicationService;
     }
 
+    @Operation(summary = "Get all applications", description = "Retrieve a list of all applications")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of applications"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping
     public List<ApplicationDto> getApplications() {
         return applicationService.getAllApplications();
